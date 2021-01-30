13e93abfab5f97522f1f60d8f21d2bd4ea48f933
EvanMaxted <evanmaxted@cmail.carleton.ca>
2021-01-30 19:12:02 -0400

    Changes

--- src/components/Button.js
+++ src/components/Button.js
@@ -14,25 +14,29 @@ import React from 'react';
 import './Button.css';
 import { Link } from 'react-router-dom';
 
-const STYLES = ['btn--primary', 'btn--outline', 'btn--test'];
+const STYLES = ['btn--primary', 'btn--outline'];
 
 const SIZES = ['btn--medium', 'btn--large'];
+
+const BUTTLINKS = ['/', '/sign-up'];
 
 export const Button = ({
   children,
   type,
   onClick,
   buttonStyle,
-  buttonSize
+  buttonSize,
+  buttonLink
 }) => {
-  const checkButtonStyle = STYLES.includes(buttonStyle)
-    ? buttonStyle
-    : STYLES[0];
+  const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0];
 
   const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
 
+  const checkButtonLink = BUTTLINKS.includes(buttonLink) ? buttonLink : BUTTLINKS[0];
+
   return (
-    <Link to='/sign-up' className='btn-mobile'>
+    // <Link to='/services' className='btn-mobile'>
+    <Link to={checkButtonLink} className='btn-mobile'>
       <button
         className={`btn ${checkButtonStyle} ${checkButtonSize}`}
         onClick={onClick}
--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -3,9 +3,13 @@ import '../../App.css';
 
 export default function Services() {
   return (
+<<<<<<< Updated upstream
     <>
   <h1 className='services'>SERVICES</h1>
   <p>Testing Testing 1 2</p>
   </>
+=======
+  <h1 className='services'>SERVICES</h1>
+>>>>>>> Stashed changes
   );
 }
