ed109a70c6206b247974229d6637a18c535afadf
simokarr <71099701+simokarr@users.noreply.github.com>
2021-01-30 17:58:08 -0500

    Changes
    Edited pictures from the original source code

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -5,42 +5,48 @@ import CardItem from './CardItem';
 function Cards() {
   return (
     <div className='cards'>
-      <h1>Check out these EPIC Destinations!</h1>
+      <h1>Our selection</h1>
       <div className='cards__container'>
         <div className='cards__wrapper'>
           <ul className='cards__items'>
             <CardItem
-              src='images/img-9.jpg'
-              text='Explore the hidden waterfall deep inside the Amazon Jungle'
-              label='Adventure'
+              src='images/img-2.JPG!D'
+              text='Top Soil'
               path='/services'
             />
             <CardItem
-              src='images/img-2.jpg'
-              text='Travel through the Islands of Bali in a Private Cruise'
-              label='Luxury'
+              src='images/img-9.jpg'
+              text='Mulch'
               path='/services'
             />
           </ul>
           <ul className='cards__items'>
             <CardItem
-              src='images/img-3.jpg'
-              text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
-              label='Mystery'
+              src='images/img-3.JPG!D'
+              text='Stone selection'
               path='/services'
             />
             <CardItem
-              src='images/img-4.jpg'
-              text='Experience Football on Top of the Himilayan Mountains'
-              label='Adventure'
+              src='images/img-8.jpg'
+              text='Sand'
+              path='/sign-up'
+            />
+            <CardItem
+              src='images/img-4.JPG!D'
+              text='Granite Boulders'
               path='/products'
             />
             <CardItem
-              src='images/img-8.jpg'
-              text='Ride through the Sahara Desert on a guided camel tour'
-              label='Adrenaline'
-              path='/sign-up'
+              src='images/img-5.jpg'
+              text='Stone dust'
+              path='/products'
             />
+            <CardItem
+              src='images/img-10.webp'
+              text='Landscaping supplies'
+              path='/products'
+            />
+
           </ul>
         </div>
       </div>
