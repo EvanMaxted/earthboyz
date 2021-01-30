9b35f31ba6474fc046c6da901eef06ff6beed6e6
EvanMaxted <evanmaxted@cmail.carleton.ca>
2021-01-30 19:28:32 -0400

    Changes

--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -3,13 +3,6 @@ import '../../App.css';
 
 export default function Services() {
   return (
-<<<<<<< Updated upstream
-    <>
   <h1 className='services'>SERVICES</h1>
-  <p>Testing Testing 1 2</p>
-  </>
-=======
-  <h1 className='services'>SERVICES</h1>
->>>>>>> Stashed changes
   );
 }
