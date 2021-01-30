a8d52d719e93855fbebedc7d70b96a8467bbe363
liquesylla <59775278+liquesylla@users.noreply.github.com>
2021-01-30 17:59:42 -0500

    Admin

--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -2,5 +2,10 @@ import React from 'react';
 import '../../App.css';
 
 export default function Services() {
-  return <h1 className='services'>SERVICES</h1>;
+  return (
+    <>
+  <h1 className='services'>SERVICES</h1>
+  <p>Testing Testing 1 2</p>
+  </>
+  );
 }
