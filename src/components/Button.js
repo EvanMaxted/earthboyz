9d4c8de66a1970b34193e38b19080c283c8366a5
Vladimir Kovacina <vladimirkovacina@yahoo.com>
2021-01-30 16:00:15 -0800

    Revert "Merge branch 'master' of https://github.com/EvanMaxted/earthboyz"
    This reverts commit 17b5491089155994adce601be8ad953d228c2c67, reversing
    changes made to cf185e7379ccfed37e4ade0d78b601785989e7c7.

--- src/components/Button.js
+++ src/components/Button.js
@@ -14,29 +14,25 @@ import React from 'react';
 import './Button.css';
 import { Link } from 'react-router-dom';
 
-const STYLES = ['btn--primary', 'btn--outline'];
+const STYLES = ['btn--primary', 'btn--outline', 'btn--test'];
 
 const SIZES = ['btn--medium', 'btn--large'];
-
-const BUTTLINKS = ['/', '/sign-up'];
 
 export const Button = ({
   children,
   type,
   onClick,
   buttonStyle,
-  buttonSize,
-  buttonLink
+  buttonSize
 }) => {
-  const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0];
+  const checkButtonStyle = STYLES.includes(buttonStyle)
+    ? buttonStyle
+    : STYLES[0];
 
   const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
 
-  const checkButtonLink = BUTTLINKS.includes(buttonLink) ? buttonLink : BUTTLINKS[0];
-
   return (
-    // <Link to='/services' className='btn-mobile'>
-    <Link to={checkButtonLink} className='btn-mobile'>
+    <Link to='/sign-up' className='btn-mobile'>
       <button
         className={`btn ${checkButtonStyle} ${checkButtonSize}`}
         onClick={onClick}
